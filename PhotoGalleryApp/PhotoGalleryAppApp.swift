049cd50461000ae365e6f3c1f50cd6f597f974be
Adrian Rodzic <adrianrodzic33@gmail.com>
2023-08-17 22:02:43 +0200

    Initial Commit

---
+++ PhotoGalleryApp/PhotoGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryAppApp.swift
+//  PhotoGalleryApp
+//
+//  Created by Adrian Rodzic on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
